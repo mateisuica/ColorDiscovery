9e74cefabfe3f7f19af2e43236657e8c5041def6
QuadroQ <matei_suica@yahoo.com>
2015-10-06 13:51:25 +0300

    Merge fix

--- mobile/src/main/java/ro/quadroq/colordiscovery/colorlist/MainActivity.java
+++ mobile/src/main/java/ro/quadroq/colordiscovery/colorlist/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SubMenu;
 import android.widget.FrameLayout;
 
